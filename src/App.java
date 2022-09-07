0139399c7dde73a469327f2d1ee0809aeb651799
Gabriel de Menezes <gabriel.gmnogueira@gmail.com>
2022-09-06 23:23:12 -0300

    Arrow function

--- src/App.java
+++ src/App.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
@@ -14,10 +15,15 @@ public class App {
         list.add(new Product("Notebook", 1200.00));
         list.add(new Product("Tablet", 450.00));
 
+        // Função anônima para implementar a interface Comparator
+        Comparator<Product> comparator = (p1, p2) -> {
+            return p1.getName().toUpperCase().compareTo(p2.getName().toUpperCase());
+        };
+
         // chamando o método sort para
         // comparar a lista através da interface
         // MyCompatador
-        list.sort((p1, p2) -> p1.getName().toUpperCase().compareTo(p2.getName().toUpperCase()));
+        list.sort(comparator);
 
         for (Product p : list) {
             System.out.println(p);
